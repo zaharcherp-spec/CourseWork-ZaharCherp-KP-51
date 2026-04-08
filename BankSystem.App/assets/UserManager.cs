ce4990fe86d953d30e12f3f3935cf65a81557cb3
Твоє Ім'я <@email>
2026-04-08 04:14:22 +0300

    refactor: rename library folders for better clarity

--- BankSystem.App/assets/UserManager.cs
+++ BankSystem.App/assets/UserManager.cs
@@ -12,4 +12,10 @@ public class UserManager
     {
         return Current.ShowCurrentBalance();
     }
+
+    public bool ChangeName(string newName)
+    {
+        Current.ChangeName(newName);
+        return true;
+    }
 }

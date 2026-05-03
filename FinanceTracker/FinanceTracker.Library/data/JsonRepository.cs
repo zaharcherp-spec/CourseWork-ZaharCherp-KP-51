2887401349266dbff071fcfcc293ebfc934c843b
Zahar <@email>
2026-05-04 00:23:58 +0300

    feat: finalize authorization system architecture

--- FinanceTracker/FinanceTracker.Library/data/JsonRepository.cs
+++ FinanceTracker/FinanceTracker.Library/data/JsonRepository.cs
@@ -13,18 +13,25 @@ public class JsonRepository<T>
         _options = new JsonSerializerOptions { WriteIndented = true };
     }
 
-    public List<T> Load()
+    public async Task<List<T>> LoadAsync()
     {
         if (!File.Exists(_filePath))
             return new List<T>();
 
-        string json = File.ReadAllText(_filePath);
-        return JsonSerializer.Deserialize<List<T>>(json, _options) ?? new List<T>();
+        try
+        {
+            string json = await File.ReadAllTextAsync(_filePath);
+            return JsonSerializer.Deserialize<List<T>>(json, _options) ?? new List<T>();
+        }
+        catch (Exception e)
+        {
+            return new List<T>();
+        }
     }
 
-    public void Save(List<T> items)
+    public async Task SaveAsync(List<T> items)
     {
         string json = JsonSerializer.Serialize(items, _options);
-        File.WriteAllText(_filePath, json);
+        await File.WriteAllTextAsync(_filePath, json);
     }
 }
--- FinanceTracker/FinanceTracker.Library/services/AuthServices.cs
+++ FinanceTracker/FinanceTracker.Library/services/AuthServices.cs
@@ -1,11 +1,49 @@
 using FinanceTracker.Library.Data;
-using FinanceTracker.Library.Models; 
+using FinanceTracker.Library.Models;
 
 namespace FinanceTracker.Library.Services;
 
 public class AuthManager
 {
     private readonly JsonRepository<User> _repository;
-    private List<User> _users;
-    
+    private List<User> _users = new();
+
+
+    public User? CurrentUser { get; private set; }
+
+    public AuthManager(JsonRepository<User> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task InitAsync()
+    {
+        _users = await _repository.LoadAsync();
+    }
+
+    public async Task<bool> RegisterAsync(string username, string password)
+    {
+        if (_users.Any(u => u.UserName == username)) return false;
+
+        var newUser = new User(username, password);
+        _users.Add(newUser);
+        await _repository.SaveAsync(_users);
+
+        return true;
+    }
+
+    public bool Login(string username, string password)
+    {
+        var user = _users.FirstOrDefault(u => u.UserName == username && u.Password == password);
+        if (user == null) return false;
+
+        CurrentUser = user;
+
+        return true;
+    }
+
+    public void Logout()
+    {
+        CurrentUser = null;
+    }
 }
